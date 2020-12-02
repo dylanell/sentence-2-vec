b70a929b6f28b94728e4f5f8608749cb382fc9d0
dylanell <dellio15@gmail.com>
2020-12-02 13:30:15 -0500

    changed naming of output files

--- model/sentence_2_vec_triplet_margin.py
+++ model/sentence_2_vec_triplet_margin.py
@@ -185,21 +185,17 @@ class Sentence2VecTripletMargin(torch.nn.Module):
     def generate_sentence_embeddings(self, data_iter, filename):
         # open file objects in append mode
         question_tok_fp = open(
-            '{}{}_{}_question_tok.txt'.format(
-                self.config['output_directory'],
-                self.config['model_type'], filename), 'w+')
+            '{}{}_question_tok.txt'.format(
+                self.config['output_directory'], filename), 'w+')
         answer_tok_fp = open(
-            '{}{}_{}_answer_tok.txt'.format(
-                self.config['output_directory'],
-                self.config['model_type'], filename), 'w+')
+            '{}{}_answer_tok.txt'.format(
+                self.config['output_directory'], filename), 'w+')
         question_vec_fp = open(
-            '{}{}_{}_question_vec.txt'.format(
-                self.config['output_directory'],
-                self.config['model_type'], filename), 'w+')
+            '{}{}_question_vec.txt'.format(
+                self.config['output_directory'], filename), 'w+')
         answer_vec_fp = open(
-            '{}{}_{}_answer_vec.txt'.format(
-                self.config['output_directory'],
-                self.config['model_type'], filename), 'w+')
+            '{}{}_answer_vec.txt'.format(
+                self.config['output_directory'], filename), 'w+')
 
         print('[INFO]: writing \'{}\' sentence embeddings...'
               .format(filename))
